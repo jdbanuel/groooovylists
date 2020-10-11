f04b06de3d61aa47cd6781548ece4e9bf7cc52e3
jdbanuel <jamesdb1995@gmail.com>
2020-10-11 02:11:41 -0700

    Create TrackList
    Use map to render list of track

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Track from '../Track/Track';
+
+function TrackList(props) {
+	return (
+		<div className="TrackList">
+			{props.tracks.map((track) => {
+				return (
+					<Track
+						name={track.name}
+						artist={track.artist}
+						album={track.album}
+						id={track.id}
+					/>
+				);
+			})}
+		</div>
+	);
+}
+
+export default TrackList;
