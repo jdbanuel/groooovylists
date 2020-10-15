2dc9942eee6ecd6e0d84b9823f082553ebaf8958
jdbanuel <jamesdb1995@gmail.com>
2020-10-14 23:24:18 -0700

    Change id to key to prevent React error in console

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Track from '../Track/Track';
+import './TrackList.css';
 
 function TrackList(props) {
 	return (
 		<div className="TrackList">
 			{props.tracks.map((track) => {
 				return (
-					<Track
+					<Track className="Track"
 						name={track.name}
 						artist={track.artist}
 						album={track.album}
-						id={track.id}
+						key={track.id}
 					/>
 				);
 			})}
