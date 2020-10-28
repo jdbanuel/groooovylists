66896af1cf31587a90b7d0985cc69b50c9b6d1aa
jdbanuel <jamesdb1995@gmail.com>
2020-10-27 19:45:29 -0700

    create local function that calls addTrack

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,9 +4,13 @@ import './Track.css';
 function Track(props) {
 	let isRemoval = false;
 
+	function addTrack() {
+		props.onAdd({name: props.name, artist: props.artist, album: props.album, id: props.id});
+	}
+
 	function renderAction() {
 		if (!isRemoval) {
-			return <button className="Track-action">+</button>;
+			return <button className="Track-action" onClick={addTrack}>+</button>;
 		} else {
 			return <button className="Track-action">-</button>;
 		}
