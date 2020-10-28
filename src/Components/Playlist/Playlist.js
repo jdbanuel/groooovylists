d4d332be8e80841eb1e7be574a4d874e88c42936
jdbanuel <jamesdb1995@gmail.com>
2020-10-27 21:18:29 -0700

    Passing App functions to child components
    Add name change functionality

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -5,8 +5,8 @@ import TrackList from '../TrackList/TrackList';
 function Playlist(props) {
 	return (
 		<div className="Playlist">
-			<input defaultValue={'New Playist'} />
-			<TrackList tracks={props.tracklist} onAdd={props.onAdd} isRemoval={false} />
+			<input defaultValue={'New Playist'} value={props.playlistName} onChange={props.handleChange} />
+			<TrackList tracks={props.tracklist} onAdd={props.onAdd} onRemove={props.onRemove} isRemoval={true} />
 			<button className="Playlist-save">SAVE TO SPOTIFY</button>
 		</div>
 	);
