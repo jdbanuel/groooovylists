8f39d2f8ab2b7d46801cc1e41d2c7e692c5b1370
jdbanuel <jamesdb1995@gmail.com>
2020-11-27 20:03:45 -0800

    Add uri prop to Track component

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,33 +2,52 @@ import React from 'react';
 import './Track.css';
 
 function Track(props) {
+    function addTrack() {
+        props.onAdd({
+            name: props.name,
+            artist: props.artist,
+            album: props.album,
+            id: props.id,
+            uri: props.uri,
+        });
+    }
 
-	function addTrack() {
-		props.onAdd({name: props.name, artist: props.artist, album: props.album, id: props.id});
-	}
+    function removeTrack() {
+        props.onRemove({
+            name: props.name,
+            artist: props.artist,
+            album: props.album,
+            id: props.id,
+            uri: props.uri,
+        });
+    }
 
-	function removeTrack() {
-		props.onRemove({name: props.name, artist: props.artist, album: props.album, id: props.id});
-	}
-
-	function renderAction() {
-		if (!props.isRemoval) {
-			return <button className="Track-action" onClick={addTrack}>+</button>;
-		} else {
-			return <button className="Track-action" onClick={removeTrack}>-</button>;
-		}
-	}
-	return (
-		<div className="Track">
-			<div className="Track-information">
-				<h3> {props.name} </h3>
-				<p>
-					{props.artist} | {props.album}
-				</p>
-			</div>
-			{renderAction()}
-		</div>
-	);
+    function renderAction() {
+        if (!props.isRemoval) {
+            return (
+                <button className="Track-action" onClick={addTrack}>
+                    +
+                </button>
+            );
+        } else {
+            return (
+                <button className="Track-action" onClick={removeTrack}>
+                    -
+                </button>
+            );
+        }
+    }
+    return (
+        <div className="Track">
+            <div className="Track-information">
+                <h3> {props.name} </h3>
+                <p>
+                    {props.artist} | {props.album}
+                </p>
+            </div>
+            {renderAction()}
+        </div>
+    );
 }
 
 export default Track;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,24 +3,26 @@ import Track from '../Track/Track';
 import './TrackList.css';
 
 function TrackList(props) {
-	return (
-		<div className="TrackList">
-			{props.tracks.map((track) => {
-				return (
-					<Track className="Track"
-						name={track.name}
-						artist={track.artist}
-						album={track.album}
-						id={track.id}
-						key={track.id}
-						onAdd={props.onAdd}
-						onRemove={props.onRemove}
-						isRemoval={props.isRemoval}
-					/>
-				);
-			})}
-		</div>
-	);
+    return (
+        <div className="TrackList">
+            {props.tracks.map((track) => {
+                return (
+                    <Track
+                        className="Track"
+                        name={track.name}
+                        artist={track.artist}
+                        album={track.album}
+                        id={track.id}
+                        key={track.id}
+                        uri={track.uri}
+                        onAdd={props.onAdd}
+                        onRemove={props.onRemove}
+                        isRemoval={props.isRemoval}
+                    />
+                );
+            })}
+        </div>
+    );
 }
 
 export default TrackList;
